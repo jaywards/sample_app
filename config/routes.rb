3416443335d4b24dddcbcce3f9e9c86bf0a68fa6
Jason <jason.wardwell@gmail.com>
2013-04-02 10:14:50 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
-  root to: 'static_pages#home'
+  get "users/new"
 
-  match '/help', to: 'static_pages#help'
-  match '/about', to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
+  root to: 'static_pages#home'
+ 
+  match '/help', to: 'static_pages#help', :as => :help
+  match '/about', to: 'static_pages#about', :as => :about
+  match '/contact', to: 'static_pages#contact', :as => :contact
+  match '/signup', to: 'users#new', :as => :signup
 
 
   # The priority is based upon order of creation:
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,7 +1,3 @@
-module ApplicationHelper
-
-#Returns the full title on a per-page basis
-
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Sample App"
 	if page_title.empty?
@@ -9,7 +5,4 @@ def full_title(page_title)
 	else
 		"#{base_title} | #{page_title}"
 	end
-end
-
-
 end
