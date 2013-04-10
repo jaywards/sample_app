0c389a8a82bb780f05ee98dcd9002571c981db74
Jason <jason.wardwell@gmail.com>
2013-04-10 09:00:43 -0700

    Finish user edit, update, index, and destroy actions

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,7 +11,7 @@ describe ApplicationHelper do
 			full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
 		end
 
-		it "should not includ a bar for the home page" do
+		it "should not include a bar for the home page" do
 			full_title("").should_not =~ /\|/
 		end
 	end
